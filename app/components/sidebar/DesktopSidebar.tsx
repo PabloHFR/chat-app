f3d66458d8b57f42deb24b35225c0e4f0502aaf2
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-05-18 09:25:57 -0300

    Added hooks and DesktopSidebar component

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
+
 export function DesktopSidebar() {
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <div></div>
+    <div
+      className="
+        hidden 
+        lg:fixed 
+        lg:inset-y-0 
+        lg:left-0 
+        lg:z-40 
+        lg:w-20 
+        xl:px-6 
+        lg:overflow-y-auto 
+        lg:bg-white 
+        lg:border-r-[1px] 
+        lg:pb-4 
+        lg:flex 
+        lg:flex-col 
+        justify-between
+        "
+    >
+      <nav
+        className="
+        mt-4
+        flex
+        flex-col
+        justify-between
+      "
+      >
+        <ul
+          role="list"
+          className="
+          flex
+          flex-col
+          items-center
+          space-y-1
+        "
+        ></ul>
+      </nav>
     </div>
   );
 }
